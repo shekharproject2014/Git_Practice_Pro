3b49fcbd2bd268ed32256359ff40527dd99f9897
demo.test <demo.test@xyz.com>
2021-06-24 19:24:48 +0530

    nwe file added on 24/05

---
+++ all_transfer_statements.py
@@ -0,0 +1,14 @@
+# Number Break
+'''for i in range(10):
+    if i==7:
+        print("it's enough")
+        break
+	print(i)'''
+
+#Age Break
+age = [3,10,38,37,40,78,74,50,56]
+
+for i in age:
+    if i > 40:
+        break
+    print(i)
---
+++ while_loop.py
@@ -0,0 +1,39 @@
+#ex:- To printnumbers from 1 to 10 by using while loop.
+
+'''x = 1
+
+while x <=10:
+    print(x)
+    x = x+1'''
+
+#ex_2:- To display thr sum of the first n muber?
+
+'''n = eval(input('Enter Number:'))
+
+s = 0
+i =1
+
+while i<=n:
+    s = s+i
+    i = i+1
+print('This sum of the first ',n,'number is',s)  '''
+
+#Infinite Loop
+'''i = 0
+while True:
+    i= i+1
+    print('Tu tho gaya re bacha',i  )'''
+
+#odd and even
+
+n = 1
+i = eval(input('Enter your no,:'))
+while (n <= i):
+    if n % 2 == 0:
+        print(n,'is even')
+    else:
+        print(n,'is odd'
+                '')
+
+    n = n+1
+
